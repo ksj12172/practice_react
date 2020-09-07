de007341cd4c750bf8ab794da2e8003b3abdbec2
ksj12172 <sojeong94@naver.com>
2020-09-07 23:29:32 +0900

    react로 NodeBird SNS 만들기01

---
+++ 10/prepare/front/components/AppLayout.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Link from 'next/Link';
+import { Menu, Input, Row, Col } from 'antd';
+import 'antd/dist/antd.css'; // webpack이 css 파일도 import 할 수 있게 해준다
+import styled from 'styled-components';
+
+import UserProfile from '../components/UserProfile';
+import LoginForm from '../components/LoginForm';
+
+const SearchInput = styled(Input.Search)`
+    vertical-align:middle;
+`
+
+const AppLayout = ({ children }) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    return (
+        <div>
+            <Menu mode="horizontal">
+                <Menu.Item>
+                    <Link href="/"><a>노드버드</a></Link>
+                </Menu.Item>
+                <Menu.Item>
+                    <Link href="/profile"><a>프로필</a></Link>
+                </Menu.Item>
+                <Menu.Item>
+                    <SearchInput enterButton></SearchInput>
+                    {/* <Input.Search enterButton style={{verticalAlign:'middle'}}/> */}
+                </Menu.Item>
+                <Menu.Item>
+                    <Link href="/signup"><a>회원가입</a></Link>
+                </Menu.Item>
+            </Menu>
+            <Row>
+                <Col xs={24} md={6}>
+                    {isLoggedIn ? <UserProfile /> : <LoginForm />}
+                </Col>
+                <Col xs={24} md={12}>{children} </Col>
+                <Col xs={24} md={6}>
+                    <a href="https://www.zerocho.com" target="_blank"
+                        rel="noreferrer noopener">Made by ZeroCho</a>
+                </Col>
+            </Row>
+            {children}
+        </div>
+    )
+}
+
+AppLayout.propTypes = {
+    children:PropTypes.node.isRequired,
+}
+export default AppLayout;
